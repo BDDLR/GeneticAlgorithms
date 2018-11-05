62911be0d8e7f689f4e76555896f8b992c33d0cd
BDDLR <bryan.dominguez.delarosa@gmail.com>
2018-11-05 08:18:18 -0600

    Practica 6

--- Practica6/main.cpp
+++ Practica6/main.cpp
@@ -84,6 +84,44 @@ bitset<BITS_PER_INDIVIDUAL> cruzaUniforme(bitset<BITS_PER_INDIVIDUAL> &p1, bitse
 
 }
 
+char * cruzaAcentuada(char * p1, char * p2){
+	int band = 0;
+	int i = 0;
+	char *aux = (char *)malloc(sizeof(char)*29);
+
+	while(i <= 29){
+		//Se asigna de p1
+		if (band == 0)
+		{
+			if(*p1 != '!' && *p2 != '!'){
+				aux[i] = *p1;
+			}
+			else{
+				aux[i] = ' ';
+				band = 1;
+			}
+			p1++;
+			p2++;
+		}
+		//Se asigna de p2
+		else
+		{
+			if(*p1 != '!' && *p2 != '!'){
+				aux[i] = *p2;
+			}
+			else{
+				aux[i] = ' ';
+				band = 0;
+			}
+			p1++;
+			p2++;
+		}
+		i++;
+	}
+
+	return aux;
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -91,23 +129,22 @@ int main(int argc, char const *argv[])
 	bitset<BITS_PER_INDIVIDUAL> p2;
 	bitset<BITS_PER_INDIVIDUAL> h1;
 	bitset<BITS_PER_INDIVIDUAL> h2;
+	char p1_a [29] = {'a',' ','a',' ','a',' ','a',' ','a',' ','a',' ','a','!','b',' ','b',' ','b',' ','b',' ','b',' ','b',' ','b',' '};
+	char p2_a [29] = {'c',' ','c',' ','c',' ','c','!','d',' ','d',' ','d',' ','d',' ','d',' ','d','!','e',' ','e',' ','e',' ','e',' '};
+	char *h1_a = (char *)malloc(sizeof(char)*29);
+	char *h2_a = (char *)malloc(sizeof(char)*29);
 
 	srand (time(NULL));
 
 	fillIndividuals(&p1);
 	fillIndividuals(&p2);
 
-	
 
 	int op = 0;
 
-	while(op != 4)
+	while(op != 5)
 	{
-		cout << "Padre 1: ";
-		printIndividuals(p1);
-		cout << "Padre 2: ";
-		printIndividuals(p2);
-		cout << endl;
+		cout << "---------------------------------" << endl;
 		cout << "1) Cruza por un punto" << endl;
 		cout << "2) Cruza de dos punto" << endl;
 		cout << "3) Cruza uniforme" << endl;
@@ -119,6 +156,11 @@ int main(int argc, char const *argv[])
 
 		switch(op){
 			case 1:
+				cout << "Padre 1: ";
+				printIndividuals(p1);
+				cout << "Padre 2: ";
+				printIndividuals(p2);
+				cout << endl;
 				cout << "Punto de cruza: 4" << endl;
 				h1 = cruzaUnPunto(p1, p2, 5);
 				h2 = cruzaUnPunto(p2, p1, 5);
@@ -130,6 +172,11 @@ int main(int argc, char const *argv[])
 				cout << endl;
 			break;
 			case 2:
+				cout << "Padre 1: ";
+				printIndividuals(p1);
+				cout << "Padre 2: ";
+				printIndividuals(p2);
+				cout << endl;
 				cout << "Puntos de cruza: 2, 6" << endl;
 				h1 = cruzaDosPunto(p1, p2);
 				h2 = cruzaDosPunto(p2, p1);
@@ -141,6 +188,11 @@ int main(int argc, char const *argv[])
 				cout << endl;
 			break;
 			case 3:
+				cout << "Padre 1: ";
+				printIndividuals(p1);
+				cout << "Padre 2: ";
+				printIndividuals(p2);
+				cout << endl;
 				cout << "Mascara definida: 1001010110 " << endl;
 				h1 = cruzaUniforme(p1, p2);
 				h2 = cruzaUniforme(p2, p1);
@@ -152,20 +204,26 @@ int main(int argc, char const *argv[])
 				cout << endl;
 			break;
 			case 4:
+				cout << "Padre 1: ";
+				cout << p1_a << endl;
+				cout << "Padre 2: ";
+				cout << p2_a << endl;
+				h1_a = cruzaAcentuada(p1_a,p2_a);
+				h2_a = cruzaAcentuada(p2_a,p1_a);
+				cout << endl;
+				cout << "Hijo 1: ";
+				cout << h1_a << endl;
+				cout << "Hijo 2: ";
+				cout << h2_a << endl;
+				cout << endl;
 			break;
 			case 5:
 				exit(0);
 			break;
 		 	default:
 		 		cout << "Selecciona una opcion correcta " << endl;
-
-
 		}
 
 	}
-
-
-
-	
 	return 0;
 }
